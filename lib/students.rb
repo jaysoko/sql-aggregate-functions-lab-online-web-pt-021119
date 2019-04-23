57d12e047551fdaec36284b164969f99b9509300
Jay Sokolowski <jaysoko@gmail.com>
2019-04-23 02:06:56 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,11 +1,11 @@
 ## Code your solution below. Note that your SQL queries should be in quotation marks.
 
 def highest_student_gpa
-  "select * from students order by gpa asc limit 1;"
+  "select max(gpa) from students;"
 end
 
 def lowest_student_gpa
-  "select * from students order by gpa desc limit 1;"
+  "select min(gpa) from students;"
 end
 
 def average_student_gpa
